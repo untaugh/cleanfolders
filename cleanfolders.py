484ddcf0994c071bff270fc906e5ccdc3f8bed73
Oskar Rundgren <orundg@gmail.com>
2016-08-06 21:05:25 +0200

    Added story. Listing files.

--- cleanfolders.py
+++ cleanfolders.py
@@ -2,29 +2,22 @@
 
 import argparse
 import os, shutil
+from pathlib import Path
 
 class cleanfolders:
-
     trashdir = os.environ['HOME'] + '/TRASH'
-    homedir = os.environ['HOME']
+    basedir = os.environ['HOME']
 
     parser = argparse.ArgumentParser(description='clean some folders')
-    parser.add_argument('-f',  default='settings', type=argparse.FileType('r'))
+    parser.add_argument('-f', default='settings', type=argparse.FileType('r'))
     args = parser.parse_args()
     settingsfile = args.f
-    print(args)
-
-    def main():
-        pass
-        #args = parser.parse_args()
-        #settingsfile = args.infile
-        #print(args)
-        #print('this is main')
 
     def parse_file(self,lines):
         dict = {'keep':[], 'clean':[]}
         mode = 'keep'
         for l in lines:
+            l = l.strip()
             if l == 'keep:':
                 mode = 'keep'
             elif l == 'clean:':
@@ -34,26 +27,75 @@ class cleanfolders:
         return dict
 
     def check_dirs(self,dirs):
-        homedir = os.environ['HOME']
         if 'clean' in dirs:
             for d in dirs['clean']:
-                if not os.path.exists(homedir + '/' + d):
+                if not os.path.exists(self.basedir + '/' + d):
                     dirs['clean'].remove(d)
         if 'keep' in dirs:
             for d in dirs['keep']:
-                if not os.path.exists(homedir + '/' + d):
+                if not os.path.exists(self.basedir + '/' + d):
                     dirs['keep'].remove(d)
         return dirs
 
     def clean_dirs(self,dirs):
         if 'clean' in dirs:
             for d in dirs['clean']:
-                shutil.move(self.homedir + '/' + d, self.trashdir + '/' + d)
+                shutil.move(self.basedir + '/' + d, self.trashdir + '/' + d)
+
+    def process_dirs(self,dirs):
+        # add missing lists
+        if not 'keep' in dirs:
+            dirs['keep'] = []
+        if not 'keep' in dirs:
+            dirs['clean'] = []
+
+        # always keep trash
+        dirs['keep'].append(Path(self.trashdir).parts[-1])
+
+        # loop remove not wanted dirs
+        for d in Path(self.basedir).iterdir():
+            dirname = d.parts[-1]
+            if not dirname in dirs['keep'] and dirname[0] != '.':
+                if os.path.exists(self.trashdir + '/' + dirname):
+                    number = 0
+                    while os.path.exists(self.trashdir + '/' + dirname + '_' + str(number)):
+                        number = number + 1
+                    dirname = dirname + '_' + str(number)
+                shutil.move(d.as_posix(), self.trashdir + '/' + dirname)
+            if dirname in dirs['clean']:
+                d.mkdir()
+
+    def list_remove(self,dirs):
+        list = []
+        for dir in Path(self.basedir).iterdir():
+            dirname = dir.parts[-1]
+            if not dirname in dirs['keep'] + dirs['clean'] and dirname[0] != '.':
+                list.append(dir)
+        return list
+
+    def list_clean(self,dirs):
+        list = []
+        for dir in Path(self.basedir).iterdir():
+            dirname = dir.parts[-1]
+            if dirname in dirs['clean']:
+                list.append(dir)
+        return list
 
     def check_trash(self):
         if not os.path.exists(self.trashdir):
             os.mkdir(self.trashdir)
 
+def main():
+    cf = cleanfolders()
+    dirs = cf.parse_file(cf.settingsfile.readlines())
+    rlist = cf.list_remove(dirs)
+    clist = cf.list_clean(dirs)
+    print("Remove files:")
+    for dir in rlist:
+        print(dir.as_posix())
+    print("Clean folders:")
+    for dir in clist:
+        print(dir.as_posix())
 
 if __name__ == '__main__':
-    cleanfolders.main()
+    main()
--- tests.py
+++ tests.py
@@ -3,11 +3,44 @@
 import unittest
 from cleanfolders import cleanfolders
 import os
+from pathlib import Path
+import shutil
 
 class TestFunctions(unittest.TestCase):
 
+    def removedir(self,dir):
+        try:
+            os.rmdir(dir)
+        except:
+            pass
+
+    def makedir(self,dir):
+        try:
+            os.mkdir(dir)
+        except:
+            pass
+
     def setUp(self):
         self.cf = cleanfolders()
+        # create directory structure for testing
+        testdir = '/tmp/cftesting'
+        trashdir = testdir + '/TRASH'
+        self.makedir(testdir)
+        self.makedir(trashdir)
+        dirs = ['Test1', 'Test2','Test3']
+        files = ['file1.txt','file2.png','file3.dat']
+        for d in dirs:
+            self.makedir(testdir + '/' + d)
+            for f in files:
+                p = Path(testdir + '/' + d + '/' + f)
+                p.touch()
+        # set directories in cleanfolders
+        self.cf.basedir = testdir
+        self.cf.trashdir = trashdir
+
+    def tearDown(self):
+        testdir = '/tmp/cftesting'
+        shutil.rmtree(testdir)
 
     def test_argparse(self):
         self.assertTrue(1)
@@ -21,14 +54,14 @@ class TestFunctions(unittest.TestCase):
         ret = self.cf.parse_file(['clean:', 'Desktop', 'keep:', 'Music', 'clean:','Downloads'])
         self.assertEqual(ret['keep'],['Music'])
         self.assertEqual(ret['clean'],['Desktop','Downloads'])
+        ret = self.cf.parse_file(['clean:\r\n', ' Cat\n', 'keep:', '\tDog \r', 'clean:\n\r','  Cow \t'])
+        self.assertEqual(ret['keep'],['Dog'])
+        self.assertEqual(ret['clean'],['Cat','Cow'])
 
     def test_check_file_dict(self):
-        home = os.environ['HOME']
+        home = self.cf.basedir
         testdir = home + '/CFTEST1'
-        try:
-            os.mkdir(testdir)
-        except:
-            pass
+        self.makedir(testdir)
         dirs1 = {'clean':['CFTEST1']}
         dirs2 = {'clean':['CFTEST2'],'keep':['CFTEST3']}
         ret1 = self.cf.check_dirs(dirs1)
@@ -40,43 +73,106 @@ class TestFunctions(unittest.TestCase):
     def test_trash_dir(self):
         savetrashdir = self.cf.trashdir
         self.cf.trashdir = '/tmp/testtrash'
-        try:
-            os.rmdir(self.cf.trashdir)
-        except:
-            pass
+        self.removedir(self.cf.trashdir)
         self.assertFalse(os.path.exists(self.cf.trashdir))
         self.cf.check_trash()
         self.assertTrue(os.path.exists(self.cf.trashdir))
-        try:
-            os.rmdir(self.cf.trashdir)
-        except:
-            pass
+        self.removedir(self.cf.trashdir)
         self.cf.trashdir = savetrashdir
 
+    def test_keep_dirs(self):
+        settings = 'keep:\nTest2\nTest3'
+        ret = self.cf.parse_file(settings.splitlines())
+        self.cf.process_dirs(ret)
+        self.assertFalse(os.path.exists(self.cf.basedir + '/Test1'))
+        self.assertTrue(os.path.exists(self.cf.trashdir + '/Test1'))
+        self.assertTrue(os.path.exists(self.cf.basedir + '/Test2'))
+        self.assertTrue(os.path.exists(self.cf.basedir + '/Test3'))
+
+    def test_keep_none(self):
+        settings = ''
+        ret = self.cf.parse_file(settings.splitlines())
+        self.cf.process_dirs(ret)
+        self.assertFalse(os.path.exists(self.cf.basedir + '/Test1'))
+        self.assertTrue(os.path.exists(self.cf.trashdir + '/Test1'))
+        self.assertFalse(os.path.exists(self.cf.basedir + '/Test2'))
+        self.assertFalse(os.path.exists(self.cf.basedir + '/Test3'))
+
     def test_clean_dirs(self):
-        home = os.environ['HOME']
-        testdir = home + '/CFTEST1'
-        try:
-            os.rmdir(self.cf.trashdir + '/CFTEST1')
-        except:
-            pass
-        try:
-            os.mkdir(testdir)
-        except:
-            pass
-        dirs1 = {'clean':['CFTEST1']}
-        self.assertTrue(os.path.exists(testdir))
-        self.cf.clean_dirs(dirs1)
-        self.assertFalse(os.path.exists(testdir))
-        self.assertTrue(os.path.exists(self.cf.trashdir + '/CFTEST1'))
-        try:
-            os.rmdir(testdir)
-        except:
-            pass
-        try:
-            os.rmdir(self.cf.trashdir + '/CFTEST1')
-        except:
-            pass
+        settings = 'clean:\nTest1'
+        ret = self.cf.parse_file(settings.splitlines())
+        self.cf.process_dirs(ret)
+        self.assertFalse(os.path.exists(self.cf.basedir + '/Test2'))
+        self.assertFalse(os.path.exists(self.cf.basedir + '/Test3'))
+        c = 0
+        for i in Path(self.cf.basedir + '/Test1').iterdir(): c = c + 1
+        self.assertEqual(0,c)
+
+    def test_clean_and_keep(self):
+        settings = 'clean:\nTest2\nkeep:\nTest1'
+        ret = self.cf.parse_file(settings.splitlines())
+        self.cf.process_dirs(ret)
+        self.assertFalse(os.path.exists(self.cf.basedir + '/Test3'))
+        self.assertTrue(os.path.exists(self.cf.trashdir + '/Test3'))
+        self.assertTrue(os.path.exists(self.cf.basedir + '/Test1'))
+        self.assertTrue(os.path.exists(self.cf.basedir + '/Test2'))
+        c = 0
+        for i in Path(self.cf.basedir + '/Test2').iterdir(): c = c + 1
+        self.assertEqual(0,c)
+
+    def test_move_duplicate(self):
+        os.mkdir(self.cf.trashdir + '/Test3')
+        os.mkdir(self.cf.trashdir + '/Test2')
+        os.mkdir(self.cf.trashdir + '/Test2_0')
+        settings = 'keep:\nTest1'
+        ret = self.cf.parse_file(settings.splitlines())
+        self.cf.process_dirs(ret)
+        self.assertTrue(os.path.exists(self.cf.trashdir + '/Test3'))
+        self.assertTrue(os.path.exists(self.cf.trashdir + '/Test3_0'))
+        self.assertTrue(os.path.exists(self.cf.trashdir + '/Test2_1'))
+
+    def test_hidden_files(self):
+        os.mkdir(self.cf.basedir + '/.hidden')
+        self.assertTrue(os.path.exists(self.cf.basedir + '/.hidden'))
+        settings = 'keep:\nTest1'
+        ret = self.cf.parse_file(settings.splitlines())
+        self.cf.process_dirs(ret)
+        self.assertTrue(os.path.exists(self.cf.basedir + '/.hidden'))
+
+    def test_list_remove(self):
+        settings = ''
+        ret = self.cf.parse_file(settings.splitlines())
+        list = self.cf.list_remove(ret)
+        self.assertTrue(Path(self.cf.basedir + '/Test2') in list)
+        self.assertTrue(Path(self.cf.basedir + '/Test3') in list)
+        self.assertTrue(Path(self.cf.basedir + '/Test1') in list)
+        settings = 'keep:\nTest1\nTest2'
+        ret = self.cf.parse_file(settings.splitlines())
+        list = self.cf.list_remove(ret)
+        self.assertFalse(Path(self.cf.basedir + '/Test2') in list)
+        self.assertTrue(Path(self.cf.basedir + '/Test3') in list)
+        self.assertFalse(Path(self.cf.basedir + '/Test1') in list)
+        settings = 'keep:\nTest1\nclean:\nTest3'
+        ret = self.cf.parse_file(settings.splitlines())
+        list = self.cf.list_remove(ret)
+        self.assertTrue(Path(self.cf.basedir + '/Test2') in list)
+        self.assertFalse(Path(self.cf.basedir + '/Test3') in list)
+        self.assertFalse(Path(self.cf.basedir + '/Test1') in list)
+
+    def test_list_clean(self):
+        settings = 'clean:\nTest3\nTest2'
+        ret = self.cf.parse_file(settings.splitlines())
+        list = self.cf.list_clean(ret)
+        self.assertTrue(Path(self.cf.basedir + '/Test2') in list)
+        self.assertTrue(Path(self.cf.basedir + '/Test3') in list)
+        self.assertFalse(Path(self.cf.basedir + '/Test1') in list)
+        settings = 'clean:\nTest1\nkeep:\nTest2'
+        ret = self.cf.parse_file(settings.splitlines())
+        list = self.cf.list_clean(ret)
+        self.assertFalse(Path(self.cf.basedir + '/Test2') in list)
+        self.assertFalse(Path(self.cf.basedir + '/Test3') in list)
+        self.assertTrue(Path(self.cf.basedir + '/Test1') in list)
+
 
 if __name__ == '__main__':
     unittest.main()
